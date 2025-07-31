e666095be30679c26f8c4efce91f04351a0d4753
oqid <sdal739@aucklanduni.ac.nz>
2025-07-31 15:14:43 +1200

    :speech_balloon: added notes to fsm

--- fsm_switch.c
+++ fsm_switch.c
@@ -1,16 +1,53 @@
 // fsm_switch.c
 #include <stdint.h>
+#include <inttypes.h>
 
 #define STATE_FAULT 0
 #define STATE_IDLE 1
 #define STATE_MOVING 2
 #define STATE_HOMING 3
 
+// todo buttons and led definitions
+
 volatile uint8_t current_state = STATE_IDLE;
+volatile uint8_t flag_additional_delay;
+
+// todo we may need to debounce the limit switch
+// we also need the limit switch to enter both FAULT and reset encoders for homing... ?
+// global variables to 'measuring tape' the distances of the gantry every time it HOMES like how a CNC machine probes a part
+
+int main() {
+
+    cli();
+    DDRB |= (1 << DDB5);
+    DDRD &= ~(1 << DDD2);
+    flag_additional_delay = 0;
+    EIMSK |= (1 << INT0);
+    EICRA |= (1 << ISC00);
+    sei();
+
+    while (1) {
+        switch (current_state) {
+            case STATE_FAULT:
+                // blink LED every 50ms
+                break;
+            case STATE_IDLE:
+                // blink LED every 500ms
+                break;
+            case STATE_MOVING:
+                break;
+            case STATE_HOMING:
+                break;
+        }
 
 
 
 
-ISR(){
+
+    }
+}
+
+
+ISR(INT0_vect) {
 
 }
